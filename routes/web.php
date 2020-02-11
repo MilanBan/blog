849fc0249e7835fd8d33e9feeda5e9645f6fb675
Milan Bancevic <m.bancevic@gmail.com>
2020-02-11 09:46:59 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); 
 });
 
 Route::get('/posts', ['as' => 'all-posts', 'uses' => 'PostController@index']);
